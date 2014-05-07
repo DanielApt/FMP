02bf26dc1bec29d5f297f57554c250f6b9bb2c66
Daniel Apt <info@iamapt.com>
2014-05-07 16:23:05 +0100

    Create basic PHP templates

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php require 'templates/header.php';?>
+
+<h1>Hello World</h1>
+<p>More information</p>
+
+<?php require 'templates/footer.php';?>
---
+++ templates/header.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Time perception test</title>
+</head>
+<body>

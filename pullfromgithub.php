5d1f915c3ac9f16ddde47f4c7f6ef7a4a6df8096
Daniel Apt <info@iamapt.com>
2014-05-17 17:36:22 +0100

    Revert 23a50a5..a4cbbca
    This rolls back to commit 23a50a5b3604defd900877444e167d0c50adbc35.

--- pullfromgithub.php
+++
@@ -1,9 +0,0 @@
-<?php
- 
-// Use in the "Post-Receive URLs" section of your GitHub repo.
- 
-if ( $_POST['payload'] ) {
-  shell_exec( 'cd /www/FMP && git reset --hard HEAD && git pull' );
-}
-?>
-Hi

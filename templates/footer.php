bdbe016d49b5810858e8a4127807cce0a39f0491
Daniel Apt <info@iamapt.com>
2014-05-17 21:16:08 +0100

    Add a footer with credit

--- templates/footer.php
+++ templates/footer.php
@@ -1,3 +1,6 @@
+		<div class="col-md-12" id="footer">
+			<small>An experiment by <a href="http://twitter.com/DanielApt" target="_blank">Daniel Apt</a> and <a href="http://twitter.com/BlytheAis" target="_blank">Blythe de Gruchy</a></small>
+		</div>
 	</div>
 </body>
 </html>

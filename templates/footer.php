a8149fac675e3ca66e01f0968cb9c2df47f36e08
Daniel Apt <info@iamapt.com>
2014-05-18 16:57:56 +0100

    Hide footer in Mobile

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,4 @@
-		<div class="col-md-12" id="footer">
+		<div class="col-md-12 hidden-xs" id="footer">
 			<small>An experiment by <a href="http://twitter.com/DanielApt" target="_blank">Daniel Apt</a> and <a href="http://twitter.com/BlytheAis" target="_blank">Blythe de Gruchy</a></small>
 		</div>
 	</div>
